928ad99e8a7972f43d6633595d6656a8a07f6b91
CyMas <cymas404@gmail.com>
2017-02-22 07:25:58 +0700

    fix

--- database/migrations/2017_02_04_075602_create_pesanans_table.php
+++ database/migrations/2017_02_04_075602_create_pesanans_table.php
@@ -16,7 +16,7 @@ class CreatePesanansTable extends Migration
             $table->increments('id');
             $table->string('nama_pemesan');
             $table->string('email_pemesan');
-            $table->integer('phone_pemesan');
+            $table->string('phone_pemesan');
             $table->text('keterangan_pemesan');
             $table->integer('kode');
             $table->string('nama_produk');
